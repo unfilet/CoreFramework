a2956f82e2991e11c9068faac0463d3bfc2a7c96
unfilet <unfilet@gmail.com>
2019-11-04 18:26:26 +0200

    Create canvas for tk2dCamera

---
+++ Plugins/TK2DROOT/tk2dCanvas.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+[ExecuteInEditMode]
+public class tk2dCanvas : MonoBehaviour
+{
+    [SerializeField]
+    tk2dCamera tk2dCamera = null;
+
+    private new RectTransform transform => base.transform as RectTransform;
+
+    void Start() => UpdateTransform();
+    public void ForceUpdateTransform() => UpdateTransform();
+    void LateUpdate() => UpdateTransform();
+
+    void UpdateTransform()
+    {
+        // Break out if anchor camera is not bound
+        if (tk2dCamera == null)
+            return;
+
+        Rect rect = tk2dCamera.ScreenExtents;
+
+        transform.anchorMin = Vector2.zero;
+        transform.anchorMax = Vector2.one;
+        transform.anchoredPosition = Vector2.zero;
+        transform.sizeDelta = rect.size;
+
+        transform.position =  new Vector3(rect.center.x, rect.center.y, transform.position.z);
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate() => UpdateTransform();
+#endif
+}
