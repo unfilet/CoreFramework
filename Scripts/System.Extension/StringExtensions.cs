2298d004a42d1b101cca2e131ba4cecb64131326
unfilet <unfilet@gmail.com>
2020-01-08 19:30:07 +0200

    add string contains wiht comparison;

---
+++ Scripts/System.Extension/StringExtensions.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public static class StringExtensions
+{
+    public static bool Contains(this string source, string toCheck, StringComparison comp)
+        => source?.IndexOf(toCheck, comp) >= 0;
+}
