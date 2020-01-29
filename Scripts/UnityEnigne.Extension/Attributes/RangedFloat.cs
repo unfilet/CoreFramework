29c00b49cbaea75ac5f77f1cbb631d701d3891e0
unfilet <unfilet@gmail.com>
2020-01-29 20:30:55 +0200

    Update ranged float

--- Scripts/UnityEnigne.Extension/Attributes/RangedFloat.cs
+++ Scripts/UnityEnigne.Extension/Attributes/RangedFloat.cs
@@ -3,15 +3,19 @@
 [Serializable]
 public struct RangedFloat
 {
+    public static RangedFloat Value(float min = float.MinValue, float max = float.MaxValue)
+        => new RangedFloat(min,max);
+
     public float minValue;
     public float maxValue;
 
-    public float Length {
-        get { return maxValue - minValue; }
-    }
+    public float Length => maxValue - minValue;
 
     public RangedFloat(float min = 1, float max = 1)
     {
         minValue = min; maxValue = max;
     }
+
+    public bool InRange(float value)
+        => minValue <= value && value <= maxValue;
 }
